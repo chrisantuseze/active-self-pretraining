8ed6dfc67eef564cee4f9444305561f33b0cf49b
Chrisantus Eze <ezechris30@gmail.com>
2022-11-04 16:15:24 -0500

    added a logic to split data

--- datautils/finetune_dataset.py
+++ datautils/finetune_dataset.py
@@ -1,7 +1,9 @@
 import os
 import torch
+from torch.utils.data import random_split
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+from datautils.dataset_enum import DatasetType
 
 from models.utils.commons import get_params
 from models.utils.training_type_enum import TrainingType
@@ -15,14 +17,10 @@ class Finetune():
         self.image_size = params.image_size
         self.batch_size = params.batch_size
 
-    def get_loader(self):
-        traindir = os.path.join(self.dir, 'train')
-        valdir = os.path.join(self.dir, 'val')
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
 
-        train_dataset = datasets.ImageFolder(
-            traindir,
+    def split_dataset(self, normalize):
+        dataset = datasets.ImageFolder(
+            self.dir,
             transforms.Compose([
                 transforms.RandomResizedCrop(224),
                 transforms.RandomHorizontalFlip(),
@@ -30,14 +28,40 @@ class Finetune():
                 normalize,
             ]))
 
-        val_dataset = datasets.ImageFolder(
-            valdir,
-            transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                normalize,
-            ]))
+        train_size = int(0.8 * len(dataset))
+        val_size = len(dataset) - train_size
+
+        train_ds, val_ds = random_split(dataset=dataset, lengths=[train_size, val_size])
+        return train_ds, val_ds
+
+    def get_loader(self):
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+        if self.args.finetune_dataset == DatasetType.IMAGENET.value:
+            traindir = os.path.join(self.dir, 'train')
+            valdir = os.path.join(self.dir, 'val')
+
+            train_dataset = datasets.ImageFolder(
+                traindir,
+                transforms.Compose([
+                    transforms.RandomResizedCrop(224),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                    normalize,
+                ]))
+
+            val_dataset = datasets.ImageFolder(
+                valdir,
+                transforms.Compose([
+                    transforms.Resize(256),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    normalize,
+                ]))
+
+        else:
+            train_dataset, val_dataset = self.split_dataset(normalize)
 
         train_loader = torch.utils.data.DataLoader(
                             train_dataset, 
