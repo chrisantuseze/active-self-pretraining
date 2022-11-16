d538cc18fa36fa97ccc53b954f6e926feafd0bb8
Chrisantus Eze <ezechris30@gmail.com>
2022-11-16 09:21:19 -0600

    update

--- datautils/finetune_dataset.py
+++ datautils/finetune_dataset.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import torchvision
 from torch.utils.data import random_split
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
@@ -37,7 +38,7 @@ class Finetune():
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
-        if self.args.finetune_dataset == DatasetType.IMAGENET.value or self.args.finetune_dataset == DatasetType.CIFAR10.value:
+        if self.args.finetune_dataset == DatasetType.IMAGENET.value:
             traindir = os.path.join(self.dir, 'train')
             valdir = os.path.join(self.dir, 'val')
 
@@ -58,6 +59,19 @@ class Finetune():
                     transforms.ToTensor(),
                     normalize,
                 ]))
+
+        elif self.args.finetune_dataset == DatasetType.CIFAR10.value:
+            train_dataset = torchvision.datasets.CIFAR10(
+                self.dir,
+                train=True,
+                download=True,
+                transform=transforms)
+
+            val_dataset = torchvision.datasets.CIFAR10(
+                self.dir,
+                train=False,
+                download=True,
+                transform=transforms)
 
         else:
             train_dataset, val_dataset = self.split_dataset(normalize)
