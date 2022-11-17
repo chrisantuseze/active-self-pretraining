1ffa5c52ec0efddf0912150596e929f1c93e549f
Chrisantus Eze <ezechris30@gmail.com>
2022-11-16 23:15:28 -0600

    update

--- rough.py
+++ rough.py
@@ -1,20 +1,6 @@
-
-# import torch
-# from numba import cuda
-
-# def free_gpu_cache():
-#     print("Initial GPU Usage")
-
-#     torch.cuda.empty_cache()
-
-#     cuda.select_device(0)
-#     cuda.close()
-#     cuda.select_device(0)
-
-#     print("GPU Usage after emptying the cache")
-
-# free_gpu_cache()  
-# 
-
-import subprocess
-subprocess.Popen(["python","main.py"])
+#!/usr/bin/env python3
+import sys
+from zipfile import PyZipFile
+for zip_file in sys.argv[1:]:
+    pzf = PyZipFile(zip_file)
+    pzf.extractall()
