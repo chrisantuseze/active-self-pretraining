cda30011be343e0106399ec4d63626ffede43650
Chrisantus Eze <ezechris30@gmail.com>
2022-11-16 23:27:26 -0600

    update

--- rough.py
+++ rough.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import sys
-from zipfile import PyZipFile
-for zip_file in sys.argv[1:]:
-    pzf = PyZipFile(zip_file)
-    pzf.extractall()
+# from zipfile import PyZipFile
+# for zip_file in sys.argv[1:]:
+#     pzf = PyZipFile(zip_file)
+#     pzf.extractall()
+
+import zipfile
+with zipfile.ZipFile("datasets/tiny-imagenet-200.zip", 'r') as zip_ref:
+    zip_ref.extractall("datasets/imagenet")
