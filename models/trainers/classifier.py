4ed37338dd86f6175e6c2ec8552fa80db945f48d
Chrisantus Eze <ezechris30@gmail.com>
2022-11-23 01:53:52 -0600

    update

--- models/trainers/classifier.py
+++ models/trainers/classifier.py
@@ -129,6 +129,8 @@ class Classifier:
         corrects = 0
         for step, (images, targets) in enumerate(train_loader):
             optimizer.zero_grad()
+            print(len(targets))
+            print(targets)
             images = images.to(self.args.device)
             targets = targets.to(self.args.device)
 
