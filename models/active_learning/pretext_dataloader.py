612ec49fcdae549c3cf548caba6d4dab3fdf463a
Chrisantus Eze <ezechris30@gmail.com>
2022-11-25 02:49:13 -0600

    update

--- models/active_learning/pretext_dataloader.py
+++ models/active_learning/pretext_dataloader.py
@@ -76,7 +76,7 @@ class PretextDataset(torch.utils.data.Dataset):
         # img = Image.fromarray(img)
 
         label = self.convert_string_to_ascii(path.split('/')[-2])
-        return self.transform.__call__(img, not self.is_val), self.target_transform(label)
+        return self.transform.__call__(img, not self.is_val), label
 
     def convert_string_to_ascii(self, input):
         a = list(input.encode('ascii'))
