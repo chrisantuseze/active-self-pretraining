92f346cb48bbf3c8020491171b3a9f4dc8f5c36f
Chrisantus Eze <ezechris30@gmail.com>
2022-11-25 03:48:55 -0600

    update

--- utils/commons.py
+++ utils/commons.py
@@ -157,7 +157,7 @@ def load_accuracy_file(args):
         return None
 
 def save_class_names(args, label):
-    filename = f"{args.dataset}.txt"
+    filename = f"{get_dataset_enum(args.dataset)}.txt"
     out = os.path.join(args.model_path, filename)
 
     try:
@@ -169,7 +169,7 @@ def save_class_names(args, label):
         None
 
 def load_class_names(args):
-    filename = f"{args.dataset}.txt"
+    filename = f"{get_dataset_enum(args.dataset)}.txt"
     out = os.path.join(args.model_path, filename)
 
     try:
