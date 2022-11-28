7fe35096acff540d13cca70e465192b4fd0f8331
Chrisantus Eze <ezechris30@gmail.com>
2022-11-28 00:22:53 -0600

    update

--- models/active_learning/pretext_trainer.py
+++ models/active_learning/pretext_trainer.py
@@ -103,7 +103,7 @@ class PretextTrainer():
             for step, (inputs, targets) in enumerate(loader):
                 inputs, targets = inputs.to(self.args.device), targets.to(self.args.device)
 
-                print(targets.min(), targets.max())
+                # print(targets.min(), targets.max())
                 
                 optimizer.zero_grad()
                 outputs = model(inputs)
