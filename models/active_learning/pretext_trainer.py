7b00f9b8d1322fbf535849f81503014b86146e27
Chrisantus Eze <ezechris30@gmail.com>
2022-11-28 00:27:41 -0600

    update

--- models/active_learning/pretext_trainer.py
+++ models/active_learning/pretext_trainer.py
@@ -354,7 +354,7 @@ class PretextTrainer():
 
         model.train()
 
-        epochs = 10
+        epochs = 5
         for epoch in range(epochs):
             logging.info('\nEpoch {}/{}'.format(epoch, epochs))
             logging.info('-' * 20)
