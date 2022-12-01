231e42b3c0be4e1834cb190815ea081ebfa67edc
Chrisantus Eze <ezechris30@gmail.com>
2022-12-01 08:11:29 -0600

    update

--- models/active_learning/pretext_dataloader.py
+++ models/active_learning/pretext_dataloader.py
@@ -26,6 +26,16 @@ class PretextDataLoader():
         self.training_type = training_type
         self.is_val = is_val
 
+        self.dir = self.args.dataset_dir + "/" + get_dataset_enum(self.args.target_dataset)
+
+        if is_val:
+            val_path_loss_list = []
+            img_paths = glob.glob(self.dir + '/test/*/*')[0:len(path_loss_list)]
+            for path in img_paths:
+                val_path_loss_list.append(PathLoss(path, 0))     
+
+            self.path_loss_list = val_path_loss_list 
+
         params = get_params(args, training_type)
         self.image_size = params.image_size
         self.batch_size = params.batch_size if not batch_size else batch_size
--- rough.py
+++ rough.py
@@ -3,15 +3,4 @@ import sys
 
 import zipfile
 with zipfile.ZipFile("tiny-imagenet-200.zip", 'r') as zip_ref:
-    zip_ref.extractall("imagenet")
-
-
-# self.dir = self.args.dataset_dir + "/" + get_dataset_enum(self.args.target_dataset)
-
-# if is_val:
-# val_path_loss_list = []
-# img_paths = glob.glob(self.dir + '/test/*/*')[0:len(path_loss_list)]
-# for path in img_paths:
-#     val_path_loss_list.append(PathLoss(path, 0))     
-
-# self.path_loss_list = val_path_loss_list   
+    zip_ref.extractall("imagenet")  
