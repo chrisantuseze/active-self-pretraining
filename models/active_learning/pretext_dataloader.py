5a6e00ebe78f3b965b02a5e3f9b9140213aad2c4
Chrisantus Eze <ezechris30@gmail.com>
2022-12-03 08:13:43 -0600

    update

--- models/active_learning/pretext_dataloader.py
+++ models/active_learning/pretext_dataloader.py
@@ -175,6 +175,12 @@ class MakeBatchLoader(torch.utils.data.Dataset):
             img2 = torch.rot90(img, 2, [1,2])
             img3 = torch.rot90(img, 3, [1,2])
             imgs = [img, img1, img2, img3]
+
+
+            print(imgs[rotations[0]].shape)
+            print(imgs[rotations[1]].shape)
+            print(imgs[rotations[2]].shape)
+            print(imgs[rotations[3]].shape)
             rotations = [0, 1, 2, 3]
             random.shuffle(rotations)
             return imgs[rotations[0]], imgs[rotations[1]], imgs[rotations[2]], imgs[rotations[3]], rotations[0], rotations[1], rotations[2], rotations[3], path
