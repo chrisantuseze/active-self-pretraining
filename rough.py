e80cdbad2f8de9ae4b3e797e0207a4917e41f578
Chrisantus Eze <ezechris30@gmail.com>
2022-12-02 22:39:47 -0600

    update

--- rough.py
+++ rough.py
@@ -2,5 +2,5 @@
 import sys
 
 import zipfile
-with zipfile.ZipFile("imagewang.zip", 'r') as zip_ref:
+with zipfile.ZipFile("imagenette2-160.zip", 'r') as zip_ref:
     zip_ref.extractall("imagenet")  
