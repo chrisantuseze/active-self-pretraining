569284ee2696a1453baba8119cffa32e2af716d3
Chrisantus Eze <ezechris30@gmail.com>
2022-12-03 10:53:47 -0600

    update

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def main():
 
         if args.do_al_for_ml_project:
             methods = [AL_Method.LEAST_CONFIDENCE.value, AL_Method.ENTROPY.value, AL_Method.BOTH.value]
-            sample_sizes = [3000] #30% and 10%. We already have for 60%
+            sample_sizes = [500] #30% and 10%. We already have for 60%
 
             for method in methods:
                 for sample_size in sample_sizes:
