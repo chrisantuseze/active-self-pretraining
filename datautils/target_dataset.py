123ebb07fe25e312baef5d12ca64715feb488e47
Chrisantus Eze <ezechris30@gmail.com>
2022-12-20 19:51:55 -0500

    update

--- datautils/target_dataset.py
+++ datautils/target_dataset.py
@@ -76,7 +76,6 @@ class TargetDataset():
     
 
 def get_target_pretrain_ds(args, training_type=TrainingType.BASE_PRETRAIN, is_train=True, batch_size=None):
-    # params = 
     if args.target_dataset == dataset_enum.DatasetType.CHEST_XRAY.value:
         print("using the CHEST XRAY dataset")
         return TargetDataset(args, "/chest_xray", training_type, is_train=is_train, batch_size=batch_size)
