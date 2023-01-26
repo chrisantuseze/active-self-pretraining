7ffab8654db7555a58e6c1a99edd8d1bc9a1ed34
Chrisantus Eze <ezechris30@gmail.com>
2023-01-26 13:35:51 -0600

    update

--- models/gan/dataloaders/setup_dataloader_smallgan.py
+++ models/gan/dataloaders/setup_dataloader_smallgan.py
@@ -10,7 +10,7 @@ def setup_dataloader(name, h=128, w=128, batch_size=4, num_workers=4):
     because this is for small dataset
     '''
     if name == "face":
-        img_path_list = glob.glob("./datasets/ucmerced/images/*/*")#"./data/face/*.png")
+        img_path_list = glob.glob("./data/ucmerced/images/*/*")#"./data/face/*.png")
         print(img_path_list)
     elif name=="anime":
         img_path_list = glob.glob("./data/anime/*.png")
@@ -20,13 +20,13 @@ def setup_dataloader(name, h=128, w=128, batch_size=4, num_workers=4):
     assert len(img_path_list) > 0
 
     transform = transforms.Compose([
-            transforms.Resize( min(h,w) ),
-            transforms.CenterCrop( (h,w) ),
-            transforms.ToTensor(),
-            ])
+        transforms.Resize(min(h, w)),
+        transforms.CenterCrop((h, w)),
+        transforms.ToTensor(),
+    ])
     
     img_path_list = [[path,i] for i,path in enumerate(sorted(img_path_list))]
-    dataset = ImageListDataset(img_path_list,transform=transform)
+    dataset = ImageListDataset(img_path_list, transform=transform)
     
     return DataLoader(
             [data for data in  dataset], 
