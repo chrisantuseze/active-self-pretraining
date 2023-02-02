1b8f87007e3fbb178f0ecb9b4cb7f10077b3b4a2
Chrisantus Eze <ezechris30@gmail.com>
2023-02-02 01:19:02 -0600

    update

--- models/gan/dataloaders/setup_dataloader_smallgan.py
+++ models/gan/dataloaders/setup_dataloader_smallgan.py
@@ -10,7 +10,7 @@ def setup_dataloader(name, h=128, w=128, batch_size=4, num_workers=4):
     because this is for small dataset
     '''
     if name == "face":
-        img_path_list = glob.glob("./datasets/ucmerced/images/*/*") #glob.glob("./models/gan/data/face/*.png")
+        img_path_list = glob.glob("./datasets/AnimalFace/*/*") #glob.glob("./models/gan/data/face/*.png")
     elif name=="anime":
         img_path_list = glob.glob("./models/gan/data/anime/*.png")
     else:
