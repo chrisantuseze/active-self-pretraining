b735047a3a4221d41d0d7083bf8d934445ffafac
Chrisantus Eze <ezechris30@gmail.com>
2023-02-16 21:57:53 -0600

    trying out gan from lucidrains

--- models/gan6/__init__.py
+++ models/gan6/__init__.py
@@ -1,2 +1,2 @@
-from lightweight_gan.lightweight_gan import LightweightGAN, Generator, Discriminator, Trainer, NanException
+from models.gan6.lightweight_gan import LightweightGAN, Generator, Discriminator, Trainer, NanException
 from kornia.filters import filter2d
