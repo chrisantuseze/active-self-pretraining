b63c8c85530658e964e011d08aea9e69993c04ab
Chrisantus Eze <ezechris30@gmail.com>
2023-02-26 17:11:53 -0600

    update

--- models/gan5/train.py
+++ models/gan5/train.py
@@ -245,6 +245,7 @@ def do_gen_ai(args):
     if not os.path.exists(gen_images_path):
         os.makedirs(gen_images_path)
 
+    print("Generated images path", gen_images_path)
     for i in range(32):
         generate_images(gen_args, gen_images_path, i)
 
