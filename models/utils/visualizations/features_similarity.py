1a4821769dde9c47e9ccbdcf9f4dde2f97882e91
Chrisantus Eze <ezechris30@gmail.com>
2023-03-11 21:56:52 -0600

    update

--- models/utils/visualizations/features_similarity.py
+++ models/utils/visualizations/features_similarity.py
@@ -58,6 +58,8 @@ class FeatureSimilarity():
                 optimizer.zero_grad()
                 outputs, outputs1, outputs2, outputs3 = model(inputs), model(inputs1), model(inputs2), model(inputs3)
 
+                print(outputs.shape(), targets.shape())
+
                 loss = criterion(outputs, targets)
                 loss1 = criterion(outputs1, targets1)
                 loss2 = criterion(outputs2, targets2)
