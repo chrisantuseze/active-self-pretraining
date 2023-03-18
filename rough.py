4cada9f625ec7857cf4dd8d64670477bcdd48616
Chrisantus Eze <ezechris30@gmail.com>
2023-03-18 01:38:07 -0500

    update

--- rough.py
+++ rough.py
@@ -2,8 +2,8 @@
 import sys
 
 import zipfile
-with zipfile.ZipFile("datasets/chest_xray.zip", 'r') as zip_ref:
-    zip_ref.extractall("datasets/chest_xray")  
+with zipfile.ZipFile("chest_xray.zip", 'r') as zip_ref:
+    zip_ref.extractall("chest_xray")  
 
 # arr = [3, 4, 1, 0, 6]
 # arr = arr[::-1]
