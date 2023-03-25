6e7f2b307ea58e5cf085cfa3edf730aa6c8e1fab
Chrisantus Eze <ezechris30@gmail.com>
2023-03-25 12:29:31 -0500

    update

--- models/gan1/train.py
+++ models/gan1/train.py
@@ -69,7 +69,7 @@ def do_gen_ai(args):
     dataset = 'imagenet_gan' #get_dataset_enum(args.target_dataset)
 
     # gen_images_path = os.path.join(args.dataset_dir, f'{args.base_dataset}')
-    gen_images_path = os.path.join(args.dataset_dir, f'generated_{dataset}/')
+    gen_images_path = os.path.join(args.dataset_dir, f'generated_{dataset}')
 
     if not os.path.exists(gen_images_path):
         os.makedirs(gen_images_path)
@@ -199,7 +199,7 @@ def standalone_image_gen(args):
     # model.load_state_dict(state['model'], strict=False)
     model = model.to(args.device)
 
-    gen_images_path = os.path.join(args.dataset_dir, f'{args.base_dataset}/')
+    gen_images_path = os.path.join(args.dataset_dir, f'{args.base_dataset}')
     if not os.path.exists(gen_images_path):
         os.makedirs(gen_images_path)
 
