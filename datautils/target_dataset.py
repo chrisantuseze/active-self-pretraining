98e95beedae3b03cc766cb78b49c46af51e91583
Chrisantus Eze <ezechris30@gmail.com>
2023-03-31 15:11:25 -0500

    update

--- datautils/target_dataset.py
+++ datautils/target_dataset.py
@@ -135,14 +135,14 @@ class TargetDataset():
 
 def get_target_pretrain_ds(args, training_type=TrainingType.BASE_PRETRAIN, is_train=True, batch_size=None) -> TargetDataset:
     # comment the two if's for gradually pretraining
+
+    # if training_type == TrainingType.BASE_AL:
+    #     print("using the Generated dataset after AL")
+    #     return TargetDataset(args, f"/{args.base_dataset}", TrainingType.ACTIVE_LEARNING, is_train=is_train, batch_size=batch_size)
     
-    if training_type == TrainingType.BASE_AL:
-        print("using the Generated dataset after AL")
-        return TargetDataset(args, f"/{args.base_dataset}", TrainingType.ACTIVE_LEARNING, is_train=is_train, batch_size=batch_size)
-    
-    if training_type == TrainingType.BASE_PRETRAIN:
-        print("using the Generated dataset")
-        return TargetDataset(args, f"/{args.base_dataset}", TrainingType.BASE_PRETRAIN, is_train=is_train, batch_size=batch_size)
+    # if training_type == TrainingType.BASE_PRETRAIN:
+    #     print("using the Generated dataset")
+    #     return TargetDataset(args, f"/{args.base_dataset}", TrainingType.BASE_PRETRAIN, is_train=is_train, batch_size=batch_size)
         
 
     if args.target_dataset == dataset_enum.DatasetType.CHEST_XRAY.value:
