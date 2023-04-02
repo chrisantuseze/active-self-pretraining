f52253e5dc68aea5da36bb195e0889951d6c7610
Chrisantus Eze <ezechris30@gmail.com>
2023-04-02 08:19:41 -0500

    update

--- rough.py
+++ rough.py
@@ -2,8 +2,8 @@
 import sys
 
 import zipfile
-with zipfile.ZipFile("datasets/food-101.zip", 'r') as zip_ref:
-    zip_ref.extractall("food-101")  
+with zipfile.ZipFile("datasets/UCMerced_LandUse.zip", 'r') as zip_ref:
+    zip_ref.extractall("ucmerced")  
 
 # arr = [3, 4, 1, 0, 6]
 # arr = arr[::-1]
