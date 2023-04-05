851fdef20c1ea2a4add2188a47d5fd9bbb02829e
Chrisantus Eze <ezechris30@gmail.com>
2023-04-04 20:07:43 -0500

    update

--- rough.py
+++ rough.py
@@ -2,8 +2,8 @@
 import sys
 
 import zipfile
-with zipfile.ZipFile("datasets/flowers.zip", 'r') as zip_ref:
-    zip_ref.extractall("flowers")  
+with zipfile.ZipFile("datasets/EuroSAT.zip", 'r') as zip_ref:
+    zip_ref.extractall("eurosat")  
 
 # arr = [3, 4, 1, 0, 6]
 # arr = arr[::-1]
