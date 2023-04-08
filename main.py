47edeeb2a51741f4f215b53a9b038c88559c0412
Chrisantus Eze <ezechris30@gmail.com>
2023-04-08 13:50:11 -0500

    update

--- main.py
+++ main.py
@@ -64,6 +64,10 @@ def main(args):
         classifier = Classifier(args, pretrain_level="2" if args.target_pretrain else "1")
         classifier.train_and_eval()
 
+        args.target_pretrain = False
+        classifier = Classifier(args, pretrain_level="2" if args.target_pretrain else "1")
+        classifier.train_and_eval()
+
         # datasets = [2, 4, 5, 6, 8, 10, 9] #[5, 6, 4, 8]
         # for ds in datasets:
         #     args.lc_dataset = ds
