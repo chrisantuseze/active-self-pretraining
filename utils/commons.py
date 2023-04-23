51443d575ff065dfa4c9ce2bf505cb09afc17346
Chrisantus Eze <ezechris30@gmail.com>
2023-04-23 18:07:04 -0500

    update

--- utils/commons.py
+++ utils/commons.py
@@ -43,7 +43,11 @@ def load_saved_state(args, recent=True, pretrain_level="1"):
         dataset = get_dataset_enum(args.target_dataset)
         additional_ext = get_accuracy_file_ext(args)
 
-        out = args.resume if recent and args.resume else os.path.join(
+        # out = args.resume if recent and args.resume else os.path.join(
+        #         args.model_checkpoint_path, "{}_{}_checkpoint_{}_{}.tar".format(prefix, pretrain_level, dataset, epoch_num, additional_ext)
+        #     )
+
+        out = os.path.join(
                 args.model_checkpoint_path, "{}_{}_checkpoint_{}_{}.tar".format(prefix, pretrain_level, dataset, epoch_num, additional_ext)
             )
 
