b1f23803b1a470da3dd791ab72cb31fcf0e77659
Chrisantus Eze <ezechris30@gmail.com>
2023-05-05 18:27:00 -0500

    update

--- main.py
+++ main.py
@@ -329,11 +329,11 @@ def new_uc2(args, writer): #currently running
     # bases = [16, 16, 16, 17, 17, 17, 18, 18, 18, 19, 19, 19] # A-C, A-P, A-R, C-A, C-P, C-R, P-A, P-C, P-R, R-A, R-C, R-P
     # targs = [17, 18, 19, 16, 18, 19, 16, 17, 19, 16, 17, 18]
 
-    bases = [16, 16, 16, 17, 17, 17] # A-P, A-R, C-A, C-P, C-R
-    targs = [17, 18, 19, 16, 18, 19]
+    # bases = [16, 16, 16, 17, 17, 17] # A-P, A-R, C-A, C-P, C-R
+    # targs = [17, 18, 19, 16, 18, 19]
 
-    # bases = [19, 19, 19, 18, 18, 18] # R-P, R-C, R-A, P-R, P-C, P-A
-    # targs = [18, 17, 16, 19, 17, 16]
+    bases = [19, 19, 19, 18, 18, 18] # R-P, R-C, R-A, P-R, P-C, P-A
+    targs = [18, 17, 16, 19, 17, 16]
 
     for i in range(len(bases)):
         run_sequence_new_uc2(args, writer, bases[i], targs[i])
