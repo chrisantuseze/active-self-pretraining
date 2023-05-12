c6053bd2e84058607693ed24d51a85f534603843
Chrisantus Eze <ezechris30@gmail.com>
2023-05-12 17:36:39 -0500

    update

--- datautils/target_dataset.py
+++ datautils/target_dataset.py
@@ -125,6 +125,7 @@ class TargetDataset():
             )
         
         else:
+            logging.info(f"{self.args.target_dataset}")
             if self.args.target_dataset in [dataset_enum.DatasetType.MNIST_M, dataset_enum.DatasetType.SVHN]:
                 img_path = get_images_pathlist(f'{self.args.dataset_dir}/{dataset_enum.get_dataset_enum(self.args.target_dataset)}', with_train=True)
                 path_loss_list = [PathLoss(path, 0) for path in img_path]
