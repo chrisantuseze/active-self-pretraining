5dcefd626fc6dcc0c00a0ac37bc9403785562c33
Chrisantus Eze <ezechris30@gmail.com>
2023-05-15 09:35:24 -0500

    update

--- models/active_learning/pretext_trainer.py
+++ models/active_learning/pretext_trainer.py
@@ -565,7 +565,8 @@ class PretextTrainer():
             # pretraining_sample_pool = []
             # ################################
 
-            if batch >= 0:
+            batch += 1 
+            if batch > 0:
                 logging.info(f'>> Getting best checkpoint for batch {batch + 1}')
 
                 state = simple_load_model(self.args, path=f'{batch-1}_finetuner_{self.dataset}.pth')
