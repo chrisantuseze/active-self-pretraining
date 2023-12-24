56b41bd8f73f57256e5c42cddcfc74f3bfef99c9
Chrisantus Eze <ezechris30@gmail.com>
2023-12-24 04:58:27 -0600

    update

--- models/trainers/trainer.py
+++ models/trainers/trainer.py
@@ -26,12 +26,10 @@ class Trainer:
                 momentum=args.momentum,
                 weight_decay=train_params.weight_decay,
             )
-            self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-                    self.optimizer, train_params.epochs, eta_min=1e-5
-            )
         else:
             self.optimizer = torch.optim.Adam(model.parameters(), lr=train_params.lr, weight_decay=train_params.weight_decay)
         
+        self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, train_params.epochs, eta_min=1e-5)
         self.criterion = nn.CrossEntropyLoss()
 
         self.best_model = copy.deepcopy(self.model)
