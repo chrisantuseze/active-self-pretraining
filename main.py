615af15ca4f294b64908dc31898c37426b395c50
Chrisantus Eze <ezechris30@gmail.com>
2023-12-29 00:00:45 -0600

    update

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ logging.init()
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--source_batch_size', default=4, type=int, help='')
+    parser.add_argument('--source_batch_size', default=8, type=int, help='')
     parser.add_argument('--source_image_size', default=128, type=int, help='')
-    parser.add_argument('--source_lr', default=1e-2, type=float, help='')
+    parser.add_argument('--source_lr', default=1e-3, type=float, help='')
     parser.add_argument('--source_epochs', default=600, type=int, help='')
     parser.add_argument('--source_weight_decay', default=1.0e-4, type=float, help='')
     parser.add_argument('--source_dataset', default=4, type=int, help='')
@@ -36,7 +36,7 @@ def parse_args():
     parser.add_argument('--al_weight_decay', default=5.0e-4, type=float, help='')
     parser.add_argument('--al_trainer_sample_size', default=400, type=int, help='specifies the amount of samples to be added to the training pool after each AL iteration')
     parser.add_argument('--al_sample_percentage', default=0.95, type=float, help='specifies the percentage of the samples to be used for the target pretraining')
-    parser.add_argument('--al_batches', default=8, type=int, help='')
+    parser.add_argument('--al_batches', default=10, type=int, help='specifies the number of AL iterations')
 
     parser.add_argument('--gan_batch_size', default=32, type=int, help='')
     parser.add_argument('--gan_image_size', default=64, type=int, help='')
@@ -50,7 +50,6 @@ def parse_args():
     parser.add_argument('--model_checkpoint_path', default="save/checkpoints", type=str, help='')
     parser.add_argument('--model_misc_path', default="save/misc", type=str, help='')
     parser.add_argument('--gen_images_path', default="generated", type=str, help='')
-    parser.add_argument('--object_set', default='seen', type=str, help='')
 
     parser.add_argument('--split_ratio', default=0.8, type=float, help='')
     parser.add_argument('--momentum', type=float, default=0.9, help='Momentum for SGD')
