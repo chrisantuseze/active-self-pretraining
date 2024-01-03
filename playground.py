6cde37155539a321ea111f944bd0eab53b002d78
Chrisantus Eze <ezechris30@gmail.com>
2024-01-03 14:19:13 -0600

    using officehome dataset

--- playground.py
+++ playground.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import zipfile
 
-with zipfile.ZipFile("office-31.zip", 'r') as zip_ref:
-    zip_ref.extractall("office-31")
+with zipfile.ZipFile("officehome.zip", 'r') as zip_ref:
+    zip_ref.extractall("officehome")
