bec39a28b7821c211acccb9a96b2082a22532644
Chrisantus Eze <ezechris30@gmail.com>
2024-01-03 14:15:47 -0600

    using officehome dataset

--- main.py
+++ main.py
@@ -20,14 +20,14 @@ def parse_args():
     parser.add_argument('--source_lr', default=1e-3, type=float, help='')
     parser.add_argument('--source_epochs', default=600, type=int, help='')
     parser.add_argument('--source_weight_decay', default=1.0e-4, type=float, help='')
-    parser.add_argument('--source_dataset', default=4, type=int, help='')
+    parser.add_argument('--source_dataset', default=7, type=int, help='')
 
     parser.add_argument('--target_batch_size', default=4, type=int, help='')
     parser.add_argument('--target_image_size', default=128, type=int, help='')
     parser.add_argument('--target_lr', default=1.0e-4, type=float, help='')
     parser.add_argument('--target_epochs', default=25, type=int, help='')
     parser.add_argument('--target_weight_decay', default=1.0e-4, type=float, help='')
-    parser.add_argument('--target_dataset', default=6, type=int, help='')
+    parser.add_argument('--target_dataset', default=8, type=int, help='')
 
     parser.add_argument('--al_batch_size', default=256, type=int, help='')
     parser.add_argument('--al_image_size', default=256, type=int, help='')
