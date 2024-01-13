e497785afe0c3acc86a673591f0d01f7ce39b2bd
Chrisantus Eze <ezechris30@gmail.com>
2024-01-13 02:43:44 -0600

    training d-a

--- models/self_sup/swav/swav.py
+++ models/self_sup/swav/swav.py
@@ -173,9 +173,9 @@ class SwAVTrainer():
                 # Option 2: Entropy maximization  
                 s_loss = -torch.sum(F.log_softmax(src_domain_out, dim=0))
                 t_loss = -torch.sum(F.log_softmax(tgt_domain_out, dim=0))
-                print("s_loss", s_loss, "t_loss", t_loss)
+                print("s_loss", s_loss.item(), "t_loss", t_loss.item())
                 entropy_conf_loss = 0.5 * (s_loss + t_loss)  #-torch.sum(F.log_softmax(src_domain_out, dim=1)) - torch.sum(F.log_softmax(tgt_domain_out, dim=1))
-                # print("entropy_conf_loss", entropy_conf_loss.item())
+                print("entropy_conf_loss", entropy_conf_loss.item())
 
                 domain_conf_loss = 0.5 * (conf_loss + entropy_conf_loss)
 
