0e8620e034b412e22a10bf26a1ef6ed48db70abd
Chrisantus Eze <ezechris30@gmail.com>
2024-01-17 18:02:53 -0600

    r-s

--- playground.py
+++ playground.py
@@ -10,5 +10,5 @@ with zipfile.ZipFile("domainnet/painting.zip", 'r') as zip_ref:
 with zipfile.ZipFile("domainnet/clipart.zip", 'r') as zip_ref:
     zip_ref.extractall("clipart")
 
-with zipfile.ZipFile("domainnet/real.zip", 'r') as zip_ref:
-    zip_ref.extractall("real")
+# with zipfile.ZipFile("domainnet/real.zip", 'r') as zip_ref:
+#     zip_ref.extractall("real")
