bf2707880288b798f9c5a232068ab9e9ff3c946e
Chrisantus Eze <ezechris30@gmail.com>
2024-01-22 22:02:08 -0600

    fixed mem issue

--- models/trainers/swav.py
+++ models/trainers/swav.py
@@ -164,6 +164,7 @@ class SwAVTrainer():
 
                 # entropy minimization loss
                 ent_loss = entropy_loss(output)
+                print("ent_loss", ent_loss)
 
                 # # domain confusion loss
                 # conf_loss = F.binary_cross_entropy(src_domain_out, torch.ones_like(tgt_domain_out)) + F.binary_cross_entropy(tgt_domain_out, torch.zeros_like(src_domain_out)) 
@@ -174,6 +175,7 @@ class SwAVTrainer():
                 # entropy_conf_loss *= 5e-4 * 0.5
 
                 wr_loss = weight_reg_loss(self.source_model, self.model)
+                print("wr_loss", wr_loss)
 
                 # loss += 0.6 * domain_adv_loss + 0.1 * (ent_loss + vat_loss) + 0.1 * wr_loss
                 loss += 0.1 * (ent_loss) + 0.1 * wr_loss
