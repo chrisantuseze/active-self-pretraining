4ab486eb3a8e8312b55baa56a119bcfe8e1da594
Chrisantus Eze <ezechris30@gmail.com>
2024-01-25 13:25:34 -0600

    update playground.py

--- playground.py
+++ playground.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 import zipfile
 
-# with zipfile.ZipFile("domainnet/sketch.zip", 'r') as zip_ref:
-#     zip_ref.extractall("sketch")
+with zipfile.ZipFile("sketch.zip", 'r') as zip_ref:
+    zip_ref.extractall("sketch")
 
-# with zipfile.ZipFile("domainnet/painting.zip", 'r') as zip_ref:
-#     zip_ref.extractall("painting")
+with zipfile.ZipFile("painting.zip", 'r') as zip_ref:
+    zip_ref.extractall("painting")
 
-# with zipfile.ZipFile("domainnet/clipart.zip", 'r') as zip_ref:
-#     zip_ref.extractall("clipart")
+with zipfile.ZipFile("clipart.zip", 'r') as zip_ref:
+    zip_ref.extractall("clipart")
 
-# with zipfile.ZipFile("domainnet/real.zip", 'r') as zip_ref:
-#     zip_ref.extractall("real")
+with zipfile.ZipFile("real.zip", 'r') as zip_ref:
+    zip_ref.extractall("real")
 
-with zipfile.ZipFile("office-31.zip", 'r') as zip_ref:
-    zip_ref.extractall("office-31")
+# with zipfile.ZipFile("office-31.zip", 'r') as zip_ref:
+#     zip_ref.extractall("office-31")
 
-with zipfile.ZipFile("officehome.zip", 'r') as zip_ref:
-    zip_ref.extractall("officehome")
+# with zipfile.ZipFile("officehome.zip", 'r') as zip_ref:
+#     zip_ref.extractall("officehome")
