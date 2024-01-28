a5ff9ff10286fe1568204171fc1171326814f711
Chrisantus Eze <ezechris30@gmail.com>
2024-01-28 09:20:56 -0600

    running all officehome

--- models/trainers/swav.py
+++ models/trainers/swav.py
@@ -151,22 +151,22 @@ class SwAVTrainer():
             # ============ backward and optim step ... ============
 
             #########################################################
-            if self.training_type == TrainingType.TARGET_AL and not in_domainnet(self.args.lc_dataset):
-                s_embedding, _ = self.source_model(inputs)
-                src_domain_out = self.domain_classifier(s_embedding)
-                tgt_domain_out = self.domain_classifier(embedding_)
+            # if self.training_type == TrainingType.TARGET_AL and not in_domainnet(self.args.lc_dataset):
+            #     s_embedding, _ = self.source_model(inputs)
+            #     src_domain_out = self.domain_classifier(s_embedding)
+            #     tgt_domain_out = self.domain_classifier(embedding_)
 
-                # domain adversarial loss
-                domain_adv_loss = self.domain_classifier.get_loss(src_domain_out, tgt_domain_out)
+            #     # domain adversarial loss
+            #     domain_adv_loss = self.domain_classifier.get_loss(src_domain_out, tgt_domain_out)
 
-                # virtual adversarial loss
-                # vat_loss = self.virtual_adv_loss(self.model, inputs[0])
-                # print("vat_loss", vat_loss)
+            #     # virtual adversarial loss
+            #     # vat_loss = self.virtual_adv_loss(self.model, inputs[0])
+            #     # print("vat_loss", vat_loss)
 
-                # entropy minimization loss
-                ent_loss = entropy_loss(output)
+            #     # entropy minimization loss
+            #     ent_loss = entropy_loss(output)
 
-                loss += 0.6 * domain_adv_loss + 0.1 * (ent_loss) #+ vat_loss)
+            #     loss += 0.6 * domain_adv_loss + 0.1 * (ent_loss) #+ vat_loss)
 
             #########################################################
 
